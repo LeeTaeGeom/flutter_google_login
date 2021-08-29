3407a9635c9e0871da68c7ec8404a1161756964c
LeeTaeGeom <taegeom0601@gmail.com>
2021-08-29 18:03:43 +0900

    flutter google_login(firebase)

---
+++ android/app/src/main/kotlin/com/sudar/valueup/valueuplogin/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.sudar.valueup.valueuplogin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ valueuplogin/android/app/src/main/kotlin/com/sudar/snslogin/valueuplogin/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.sudar.snslogin.valueuplogin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
